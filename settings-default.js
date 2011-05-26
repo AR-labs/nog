089a109b44eb6920cfe5b073c48aac6f3e642205
papr <pabloprietz@googlemail.com>
2011-05-26 22:59:40 +0200

    settings default

---
+++ settings-default.js
@@ -0,0 +1,21 @@
+module.exports = {
+	nog: {
+		name: "nog \u2014 node based blog system",
+		url: "",
+		port: 80
+	},
+	database: {
+		host: "", // Hostname of your Mysql-Server
+		port: 3306, // Port of your Mysql-Server
+		user: "", // User of your database
+		password: "", // The user's password for the database
+		database: "" // The name of the database
+	},
+	posttypes: {
+		0: "text",
+		1: "image",
+		2: "link",
+		3: "audio",
+		4: "video"
+	}
+};
